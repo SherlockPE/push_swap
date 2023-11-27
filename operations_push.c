41224d28f1d158ee1054f947d799dd328cb74df4
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-11-27 14:25:01 +0100

    error case in operations_push

--- operations_push.c
+++ operations_push.c
@@ -6,34 +6,20 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 11:18:48 by flopez-r          #+#    #+#             */
-/*   Updated: 2023/11/24 12:04:42 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/11/27 13:45:50 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_b(t_list **stack_a, t_list **stack_b)
-{
-	t_list	*a;
-	t_list	*b;
-
-	// Agarrar valores
-	a = *stack_a;
-	b = a->next;
-
-	// Nueva cabecera de stack_a
-	*stack_a = b;
-
-	// convertir la antigua cabecera de stack_a como la nueva cabecera de stack_b
-	ft_lstadd_front(stack_b, a);
-		// Dentro de la función se cambia el puntero de 'a' a la antigua cabecera de stack_b
-	printf("pb\n");
-}
-
 void	push_a(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*a;
 	t_list	*b;
+
+	//Caso de error
+	if (!*stack_b)
+		return ;
 
 	// Agarrar valores
 	a = *stack_b;
@@ -48,6 +34,29 @@ void	push_a(t_list **stack_a, t_list **stack_b)
 	printf("pa\n");
 	//push_b(stack_b, stack_a);
 }
+
+void	push_b(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*a;
+	t_list	*b;
+
+	//Caso de error
+	if (!*stack_a)
+		return ;
+
+	// Agarrar valores
+	a = *stack_a;
+	b = a->next;
+
+	// Nueva cabecera de stack_a
+	*stack_a = b;
+
+	// convertir la antigua cabecera de stack_a como la nueva cabecera de stack_b
+	ft_lstadd_front(stack_b, a);
+		// Dentro de la función se cambia el puntero de 'a' a la antigua cabecera de stack_b
+	printf("pb\n");
+}
+
 
 // Stack_a | Stack_b
 // 		1
