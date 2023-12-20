e378132c5591c6769e5a86a81a7820a737c2d3f8
Fabricio_lopez <flopez-r@student.42madrid.com>
2023-12-20 18:10:57 +0100

    FUNCIONA EL MERGE SORT

--- chunks_algorithm.c
+++ chunks_algorithm.c
@@ -6,45 +6,59 @@
 /*   By: flopez-r <flopez-r@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:37:16 by fabriciolop       #+#    #+#             */
-/*   Updated: 2023/12/20 14:50:36 by flopez-r         ###   ########.fr       */
+/*   Updated: 2023/12/20 18:10:31 by flopez-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-//Optimizar algoritmo de ordenacion
-static void swap(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+static void    merge_action(int *original_array, int *copy_array, int  inicio, int size)
+{
+    int i;
+    int middle;
+    int m;
+
+    middle = inicio + (size - inicio) / 2;
+    m = middle;
+
+    i = inicio;
+    while (i < size)
+    {
+        if (inicio < middle && (m >= size || copy_array[inicio] <= copy_array[m]))
+            original_array[i++] = copy_array[inicio++];
+        else
+            original_array[i++] = copy_array[m++];
+    }
 }
 
-void	merge_split(int *original_array, int *copy_array, int inicio, int size)
+static void    merge_split(int *original_array, int *copy_array, int inicio, int size)
 {
-	int	middle;
+    int    middle;
 
-	if (size <= 1);
-		return ;
-	middle = size / 2;
-	merge_split(original_array, copy_array, inicio, middle);
+    if (size - inicio <= 1)
+        return ;
+    middle = inicio + (size - inicio ) / 2;
+    merge_split(copy_array, original_array, inicio, middle);
+    merge_split(copy_array, original_array, middle, size);
+    merge_action(original_array, copy_array, inicio, size);
 }
 
-int	merge_sort(int *original_array, int size)
+int    merge_sort(int *original_array, int size)
 {
-	int	i;
-	int	*copy_array;
+    int    i;
+    int    *copy_array;
 
 	copy_array = ft_calloc(size, sizeof(int));
-	if (!copy_array)
-		return (0);
+    if (!copy_array)
+        return (0);
 
     i = -1;
     while (i++ < size - 1)
         copy_array[i] = original_array[i];
 
-	merge_split(original_array, copy_array, 0, size);
-	return (1);	
+    merge_split(original_array, copy_array, 0, size);
+    return (1);    
 }
 
 int	chunks_alg(t_list **stack_a, t_list **stack_b)
@@ -52,8 +66,6 @@ int	chunks_alg(t_list **stack_a, t_list **stack_b)
 	// int	**array_2_D;
 	int *array_1_D;
 	int size;
-	int separator;
-
 	(void)stack_b;
 	
 	size = ft_lstsize(*stack_a);
@@ -63,6 +75,14 @@ int	chunks_alg(t_list **stack_a, t_list **stack_b)
 	if (!merge_sort(array_1_D, size))
 		return (0);
 	
+	//Imprimit array
+	int i = 0;
+	while (i < size)
+	{
+		printf("Number: %d\n", array_1_D[i]);
+		i++;
+	}
+
 	// array_2_D = separate_in_chunks(array_1_D, size, separator);
 	//move_elements(stack_a, stack_b, array_2_D);
 	return (1);
